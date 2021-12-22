e40325be2dff2041c155365b8a7203d1023447a9
baskaraid <83570860+baskaraid@users.noreply.github.com>
2021-12-23 00:52:47 +0700

    Update index.php

--- WEB/index.php
+++ WEB/index.php
@@ -1,5 +1,12 @@
 <?php 
 include 'config.php';
+
+$sql = "INSERT INTO test (id, username, password) VALUES (NULL, '$username', '$password')";
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
 ?>
 
 <!doctype html>
